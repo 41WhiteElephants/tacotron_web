02d539aa5a77eeeac995f421af04888e7d4ada7d
Jakub Hurej <j.hurej@merixstudio.com>
2021-05-31 19:56:26 +0200

    init commit

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+FROM ubuntu:18.04
+
+MAINTAINER Amazon AI <sage-learner@amazon.com>
+
+RUN apt-get -y update && apt-get install -y --no-install-recommends \
+         wget \
+         tree \
+         build-essential \
+         python3-dev \
+         libevent-dev \
+	     python-all-dev \
+         python3-pip \
+         python-setuptools \
+         python3-setuptools \
+         nginx \
+         ffmpeg \
+         ca-certificates \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN ln -s -f /usr/bin/python3 /usr/bin/python
+RUN ln -s -f /usr/bin/pip3 /usr/bin/pip
+RUN pip3 install --no-cache-dir numpy==1.16.4 \
+    scipy==1.0.0  flask statsmodels pydub \
+    torch==1.5.0 torchvision==0.6.0 s3fs numba==0.48 tensorflow==1.12.2 \
+    tensorboard inflect==0.2.5 matplotlib \
+    librosa==0.6.0 Unidecode==1.0.22 pillow boto3 sagemaker gevent ipython
+
+RUN pip3 install gunicorn && \
+        rm -rf /root/.cache
+
+# Set some environment variables. PYTHONUNBUFFERED keeps Python from buffering our standard
+# output stream, which means that logs can be delivered to the user quickly. PYTHONDONTWRITEBYTECODE
+# keeps Python from writing the .pyc files which are unnecessary in this case. We also update
+# PATH so that the train and serve programs are found when the container is invoked.
+
+ENV PYTHONUNBUFFERED=TRUE
+ENV PYTHONDONTWRITEBYTECODE=TRUE
+ENV PATH="/opt/program:${PATH}"
+
+# Set up the program in the image
+COPY opt_ml /opt/ml
+COPY opt_program /opt/program
+WORKDIR /opt/program
+RUN chmod +x train
+RUN chmod +x serve
---
+++ opt_program/predictor.py
@@ -0,0 +1,117 @@
+# This is the file that implements a flask server to do inferences. It's the file that you will modify to
+# implement the scoring for your own algorithm.
+
+import os
+import boto3
+from s3fs.core import S3FileSystem
+from sagemaker import get_execution_role
+import sys
+sys.path.append('waveglow/')
+import flask
+
+import numpy as np
+import torch
+
+from tacotron2.hparams import create_hparams
+from tacotron2.train import load_model
+from tacotron2.text import text_to_sequence
+from tacotron2.waveglow import denoiser
+from pydub import AudioSegment
+from datetime import date
+import IPython.display as ipd
+
+prefix = '/opt/ml/'
+model_path = os.path.join(prefix, 'model')
+
+
+def predict(sequence):
+    hparams = create_hparams()
+    hparams.sampling_rate = 22050
+    my_bucket = "aws-linux-academy-2k10-ml-sagemaker"
+    my_file = "checkpoint_32224"
+    s3 = S3FileSystem(anon=False)
+
+    model = load_model(hparams)
+    model.load_state_dict(
+        torch.load(s3.open('{}/{}'.format(my_bucket, my_file), mode='rb'))['state_dict'])
+    _ = model.eval().half()
+    # spróbuj odpalić inferencję w module tacotron2 i tylko wrzucić wav na s3 jak się
+    # skończy inferencja
+
+    # waveglow_path = 'waveglow_256channels_universal_v5.pt'
+    # waveglow = torch.load(s3.open('{}/{}'.format(my_bucket, waveglow_path), mode='rb'))['model']
+    waveglow = torch.hub.load('nvidia/DeepLearningExamples:torchhub', 'nvidia_waveglow')
+    waveglow.eval().half()
+    for layer in waveglow.convinv:
+        layer.float()
+    denoiser = denoiser.Denoiser(waveglow)
+    mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
+    with torch.no_grad():
+        audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
+    audio_denoised = denoiser(audio, strength=0.01)[:, 0]
+    return audio_denoised
+
+
+def preprocess(text):
+    sequence = np.array(text_to_sequence(text, ['basic_cleaners']))[None, :]
+    return torch.autograd.Variable(
+        torch.from_numpy(sequence)).cuda().long()
+
+
+def write_to_s3(filename, bucket, key):
+    with open(filename, 'rb') as f: # Read in binary mode
+        return boto3.Session().resource('s3').Bucket(bucket).Object(key).upload_fileobj(f)
+
+
+# The flask app for serving predictions
+app = flask.Flask(__name__)
+
+
+@app.route('/invocations', methods=['POST'])
+def inference():
+    """Do an inference on a single batch of data. In this sample server, we take data
+    as txt, generate speech & put it on s3
+    """
+    try:
+        data = None
+        if flask.request.content_type == 'application/json':
+            data = flask.request.get_json()
+        else:
+            return flask.Response(response='This predictor only supports json data',
+                                  status=415, mimetype='text/plain')
+
+        # Preprocess
+        print("Preprocessing text!")
+        sequence = preprocess(data["text"])
+
+        # Do the prediction
+        print("Predict - getting audio!")
+        audio = predict(sequence)
+        print("Denoising audio!")
+        audio_denoised = denoiser(audio, strength=0.01)[:, 0]
+
+        audio_file = ipd.Audio(audio_denoised.cpu().numpy(), rate=22050)
+        filename = f"{date.today()}.wav"
+        audio_file = AudioSegment(audio_file.data, frame_rate=22050, sample_width=2,
+                                  channels=1)
+        print("Saving audio file!")
+        audio_file.export(filename, format="wav")
+        my_bucket = "aws-linux-academy-2k10-ml-sagemaker"
+        print("Writing file to s3")
+        write_to_s3(filename, my_bucket, filename)
+        return flask.Response(response="Audio file saved on S3 bucket", status=200,
+                              mimetype='text/plain')
+    except:
+        import traceback
+
+        return flask.Response(response=f"{traceback.format_exc()}", status=400,
+                              mimetype='text/plain')
+
+
+
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    return flask.Response(response='Pong!', status=200, mimetype='application/json')
+
+
